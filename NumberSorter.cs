dd6e510033cf12b67b3672319f904555a6e641be
Lewis Whiteman <konomira@gmail.com>
2020-11-11 14:38:43 +0000

    Update NumberSorter.cs

--- NumberSorter.cs
+++ NumberSorter.cs
@@ -7,49 +7,51 @@ using System;
 
 public class NumberSorter : MonoBehaviour
 {
+    public Button readFileButton;
+    public Button writeJSONButton;
+    public Text unsortedNumbers
+    public Text sortedNumbers;
 
-    Button readFileButton, writeJSONButton;
-    Text unsortedNumbers, sortedNumbers;
+    public string readFilePath = "Assets/Resources/Unsorted Numbers.txt";
+    public string writeFilePath = "Assets/Resources/Sorted Numbers.json";
 
-    string readFilePath = "Assets/Resources/Unsorted Numbers.txt";
-    string writeFilePath = "Assets/Resources/Sorted Numbers.json";
-
+    public JSONData data = new JSONData();
+    
     private void Start()
     {
-        readFileButton = GameObject.Find("Read File Button").GetComponent<Button>();
         readFileButton.onClick.AddListener(ReadFile);
-
-        writeJSONButton = GameObject.Find("Write JSON Button").GetComponent<Button>();
         writeJSONButton.onClick.AddListener(WriteJSON);
-
-        unsortedNumbers = GameObject.Find("Unsorted Numbers").GetComponent<Text>();
-        sortedNumbers = GameObject.Find("Sorted Numbers").GetComponent<Text>();
     }
 
-    public int[] numbers;
-    void ReadFile()
+    private void ReadFile()
     {
-        StreamReader reader = new StreamReader(readFilePath);
-        numbers = Array.ConvertAll(
-            // Reads file, splits into string[]
-            reader.ReadToEnd().Split('\n'),
-            // Parses string[] to int[]
-            s=>int.Parse(s));
+        using(var reader = new StreamReader(readFilePath))
+        {
+            data.numbers = Array.ConvertAll(
+                // Reads file, splits into string[]
+                reader.ReadToEnd().Split('\n'),
+                // Parses string[] to int[]
+                s=>int.Parse(s));
+        }
 
-        reader.Close();
+        unsortedNumbers.text = string.Join(", ", data.numbers);
 
-        unsortedNumbers.text = string.Join(", ", numbers);
+        data.numbers = data.numbers.OrderBy(n => n).ToArray();
 
-        numbers = numbers.OrderBy(n => n).ToArray();
-
-        sortedNumbers.text = string.Join(", ", numbers);
+        sortedNumbers.text = string.Join(", ", data.numbers);
     }
 
-    void WriteJSON()
+    private void WriteJSON()
     { 
-        StreamWriter writer = new StreamWriter(writeFilePath);
-        writer.Write(JsonUtility.ToJson(this));
-
-        writer.Close();
+        using(var writer = new StreamWriter(writeFilePath))
+        {
+            writer.Write(JsonUtility.ToJson(data));
+        }
+    }
+    
+    [Serializable]
+    public Class JSONData
+    {
+        public int[] numbers;
     }
 }
